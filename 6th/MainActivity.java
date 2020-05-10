f48e90ce4aacb357df0f56d593b01a0678ace1be
JJongmen <55042337+JJongmen@users.noreply.github.com>
2020-05-10 21:04:30 +0900

    Add files via upload

---
+++ 6th/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.github;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("https://jjongmen.github.io/");
+    }
+
+}
